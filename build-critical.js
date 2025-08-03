940178bdeee104c71020eeb8bb8fa4e3efba83fa
IHARNAZAROV <grizley.tery@gmail.com>
2025-08-03 13:52:40 +0300

    update

---
+++ build-critical.js
@@ -0,0 +1,28 @@
+// build-critical.js
+
+// Импортируем только нужную функцию 'generate' в фигурных скобках
+import { generate } from 'critical';
+
+// Поскольку 'critical' теперь не является объектом,
+// а 'generate' — это уже функция, то мы вызываем её напрямую
+const options = {
+  base: './', 
+  src: 'index.html', 
+  css: ['css/style.css'], 
+  inline: true, 
+  target: {
+    html: 'index-optimized.html'
+  },
+  width: 1300,
+  height: 900
+};
+
+// Вызываем функцию 'generate'
+generate(options, (err, output) => {
+  if (err) {
+    console.error('Ошибка при генерации Critical CSS:', err);
+    return;
+  }
+  
+  console.log('Critical CSS успешно сгенерирован и сохранен в index-optimized.html!');
+});
