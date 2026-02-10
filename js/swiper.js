bdb52eeb354cc687771c8441964b8edfb2374d02
unknown <grizley.tery@gmail.com>
2026-02-10 10:59:12 +0300

    refactor swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,36 +1,25 @@
-// Ждем, пока HTML полностью загрузится
 document.addEventListener('DOMContentLoaded', function () {
-  // Инициализируем Swiper
-  const swiper = new swiper('.swiper', {
+  const swiper = new Swiper('.swiper', {
 
-    // Ваши основные настройки
-    direction: 'horizontal',
+     direction: 'horizontal',
     slidesPerView: 1,
     spaceBetween: 0,
     loop: true,
     speed: 1500,
 
-    // Включаем параллакс (он просто будет работать)
+  
     parallax: true,
 
-    // Навигация (новый синтаксис)
-    navigation: {
+     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev'
     },
 
-    // Автопрокрутка (новый синтаксис)
     autoplay: {
       delay: 2500,
-      disableOnInteraction: false // Продолжать после клика
+      disableOnInteraction: false 
     }
 
-    // Если нужна пагинация (точки)
-    // 1. Добавьте <div class="swiper-pagination"></div> в HTML
-    // 2. Раскомментируйте код ниже:
-    // pagination: {
-    //   el: '.swiper-pagination',
-    //   clickable: true,
-    // },
+    
   })
 })
