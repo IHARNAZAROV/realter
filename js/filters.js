cc0687ce43b159faa3ab268d53a0ea7875caa1e8
unknown <grizley.tery@gmail.com>
2026-04-07 08:12:29 +0300

    add object

--- js/filters.js
+++ js/filters.js
@@ -95,6 +95,7 @@ const previewImages = {
   "kvartira-lida-ul-kommunisticheskaya-49":"images/objects/pic50.webp",
   "kvartira-shchuchin-ul-ostrovskogo-22":"images/objects/pic51.webp",
   "kvartira-lida-yuzhny-gorodok-13-stary-yuzhny":"images/objects/pic52.webp",
+  "dom-lida-ul-urickogo-chast-doma":"images/objects/pic53.webp",
 };
 
 /* =========================================================
